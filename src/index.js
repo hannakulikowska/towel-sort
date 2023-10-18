f8a7165fb91513ca57371c677251d1519931094e
Hanna Kulikowska <hannakulikowska.new@gmail.com>
2023-10-18 10:58:30 +0200

    feat: add solution for the towel-sort task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+  if (!Array.isArray(matrix) || matrix.length === 0 || matrix.flat().length === 0) {
+    return [];
+  }
+  const newMatrix = matrix.flatMap((element, index) => {
+    if (index % 2 === 0) {
+      return element;
+    }
+    return element.reverse();
+  });
+
+  return newMatrix.flat();
 }
